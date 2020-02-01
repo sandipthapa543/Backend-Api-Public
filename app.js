00dbf8868f9050e4a15285bd55b773dd8795bdd8
Sandip Thapa <sandip.thapa543@gmail.com>
2020-02-01 23:05:35 +0545

    morgan cors enabled

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ const express = require('express');
 
 // *import mongoose
 const mongoose = require('mongoose');
-
+const cors = require('cors')
+var morgan = require('morgan')
 //  express method
 const app = express();
-
+app.use(cors({origin: '*'}));
 // *server setup
 
 
-app.listen(3000);
+app.listen(9000);
 
 app.use(express.json());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(morgan('combined'))
 
 
 const bodyParser = require('body-parser');
